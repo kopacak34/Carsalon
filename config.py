cdf3ab13591512f8ee46a4c9529d0eae3a8038ce
KopeckyTom <kopecky4@spsejecna.cz>
2026-01-03 23:38:09 +0100

    added config and sql schema

--- config.py
+++
@@ -1,6 +0,0 @@
-DB_CONFIG = {
-    "host": "localhost",
-    "user": "root",
-    "password": "12345",
-    "database": "autosalon"
-}
--- db.py
+++ db.py
@@ -1,12 +1,38 @@
 import mysql.connector
-from mysql.connector import Error
-from config import DB_CONFIG
+import configparser
+import os
+import sys
+
+
+CONFIG_PATH = os.path.join("config", "config.ini")
+
 
 def get_connection():
+    if not os.path.exists(CONFIG_PATH):
+        print("Chyba konfigurace:")
+        print(f"Soubor {CONFIG_PATH} neexistuje.")
+        sys.exit(1)
+
+    config = configparser.ConfigParser()
+    config.read(CONFIG_PATH)
+
+    if "database" not in config:
+        print("Chyba konfigurace:")
+        print("V config.ini chybí sekce [database].")
+        sys.exit(1)
+
     try:
-        return mysql.connector.connect(**DB_CONFIG)
-    except Error as e:
-        print("Chyba připojení k databázi")
-        print("Zkontroluj config.py nebo běh MySQL serveru")
+        db_cfg = config["database"]
+
+        return mysql.connector.connect(
+            host=db_cfg.get("host"),
+            port=db_cfg.getint("port"),
+            user=db_cfg.get("user"),
+            password=db_cfg.get("password"),
+            database=db_cfg.get("database"),
+        )
+
+    except mysql.connector.Error as e:
+        print("Chyba připojení k databázi:")
         print(e)
-        exit(1)
+        sys.exit(1)
